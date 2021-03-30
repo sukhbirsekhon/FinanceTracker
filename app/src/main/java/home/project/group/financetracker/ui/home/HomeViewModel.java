f4973c82118daa906ec15c22be93654f0c876414
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2021-03-29 22:49:01 -0400

    Added a comment

--- app/src/main/java/home/project/group/financetracker/ui/home/HomeViewModel.java
+++ app/src/main/java/home/project/group/financetracker/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Welcome to the Finance Tracker App");
+        mText.setValue("Welcome to the Finance Tracker App"); //New content
     }
 
     public LiveData<String> getText() {
