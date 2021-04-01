63cc47371989efaea1f077272b5c0018c91523da
sekhonsj <sekhonsj@mail.uc.edu>
2021-03-31 20:30:31 -0400

    changed the dashboard tab name/icon to transaction/dollar sign

--- app/src/main/java/home/project/group/financetracker/ui/transaction/TransactionFragment.java
+++ app/src/main/java/home/project/group/financetracker/ui/transaction/TransactionFragment.java
@@ -1,4 +1,4 @@
-package home.project.group.financetracker.ui.dashboard;
+package home.project.group.financetracker.ui.transaction;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import home.project.group.financetracker.R;
 
-public class DashboardFragment extends Fragment {
+public class TransactionFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private TransactionViewModel transactionViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        transactionViewModel =
+                ViewModelProviders.of(this).get(TransactionViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_transaction, container, false);
+        final TextView textView = root.findViewById(R.id.text_transaction);
+        transactionViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
