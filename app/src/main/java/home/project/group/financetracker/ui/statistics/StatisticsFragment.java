3bc3c5eca4b81f8705a3100c030508907a53ed4e
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2021-05-06 16:39:41 -0400

    removed recylerview and added scrollview

--- app/src/main/java/home/project/group/financetracker/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/home/project/group/financetracker/ui/statistics/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package home.project.group.financetracker.ui.statistics;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,20 +10,31 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
-import home.project.group.financetracker.Adapter.ExpenseStatisticsAdapter;
-import home.project.group.financetracker.Adapter.RevenueStatisticsAdapter;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import home.project.group.financetracker.R;
 import home.project.group.financetracker.Utility.Theme;
 
 public class StatisticsFragment extends Fragment implements View.OnClickListener {
 
-    RecyclerView expenseRecyclerView, revenueRecyclerView;
-    private ExpenseStatisticsAdapter expenseAdapter;
-    private RevenueStatisticsAdapter revenueAdapter;
     Button expenseViewBtn, revenueViewBtn;
     LinearLayout expenseStatisticsView, revenueStatisticsView;
+    PieChart expensePieChart, revenuePieChart;
+    BarChart expenseBarChart, revenueBarChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,19 +51,174 @@ public class StatisticsFragment extends Fragment implements View.OnClickListener
         expenseStatisticsView = root.findViewById(R.id.expenseStatisticsView);
         revenueStatisticsView = root.findViewById(R.id.revenueStatisticsView);
 
-        expenseRecyclerView = root.findViewById(R.id.expenseRecyclerview);
-        revenueRecyclerView = root.findViewById(R.id.revenueRecyclerview);
+        expenseBarChart = root.findViewById(R.id.expenseBarChart);
+        expensePieChart = root.findViewById(R.id.expensePieChart);
+
+        revenueBarChart = root.findViewById(R.id.revenueBarChart);
+        revenuePieChart = root.findViewById(R.id.revenuePieChart);
 
         getData();
         return root;
     }
 
     private void getData() {
-        expenseAdapter = new ExpenseStatisticsAdapter(getActivity().getApplicationContext());
-        expenseRecyclerView.setAdapter(expenseAdapter);
 
-        revenueAdapter = new RevenueStatisticsAdapter(getActivity().getApplicationContext());
-        revenueRecyclerView.setAdapter(revenueAdapter);
+        expenseCharts();
+        revenueCharts();
+    }
+
+    private void expenseCharts() {
+        /**
+         * Pie chart
+         */
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(18.5f, "Clothing"));
+        entries.add(new PieEntry(26.7f, "Gas"));
+        entries.add(new PieEntry(24.0f, "Sports"));
+        entries.add(new PieEntry(30.8f, "Food"));
+
+        PieDataSet set = new PieDataSet(entries, "");
+
+        set.setColors(new int[]{R.color.green, R.color.yellow, R.color.red, R.color.blue}, getContext());
+        set.setValueTextSize(0.1f);
+
+        PieData data = new PieData(set);
+
+        Description description = new Description();
+        description.setEnabled(false);
+
+        CharSequence title = "EXPENSES";
+
+        expensePieChart.setData(data);
+        expensePieChart.setDescription(description);
+        expensePieChart.setCenterText(title);
+        expensePieChart.notifyDataSetChanged();
+        expensePieChart.invalidate(); // refresh
+
+        /**
+         * Bar Chart
+         */
+        List<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0f, 30f));
+        barEntries.add(new BarEntry(1f, 80f));
+        barEntries.add(new BarEntry(2f, 60f));
+        barEntries.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        barEntries.add(new BarEntry(5f, 70f));
+        barEntries.add(new BarEntry(6f, 60f));
+        BarDataSet barDataSet = new BarDataSet(barEntries, "BarDataSet");
+
+        XAxis xAxis = expenseBarChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setTextSize(8f);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelRotationAngle(-45);
+
+        YAxis right = expenseBarChart.getAxisRight();
+        right.setDrawLabels(false); // no axis labels
+        right.setDrawAxisLine(false); // no axis line
+        right.setDrawGridLines(false); // no grid lines
+        right.setDrawZeroLine(true);
+
+        YAxis left = expenseBarChart.getAxisLeft();
+        left.setDrawLabels(false); // no axis labels
+        left.setDrawAxisLine(false); // no axis line
+        left.setDrawGridLines(false); // no grid lines
+        left.setDrawZeroLine(true);
+
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.9f); // set custom bar width
+        expenseBarChart.setData(barData);
+        expenseBarChart.setFitBars(true); // make the x-axis fit exactly all bars
+        expenseBarChart.setDrawGridBackground(false);
+        expenseBarChart.setDescription(description);
+        expenseBarChart.animateXY(2000, 2000);
+        expenseBarChart.setScaleEnabled(true);
+        expenseBarChart.setDrawValueAboveBar(true);
+        expenseBarChart.setDrawGridBackground(false);
+        expenseBarChart.setDoubleTapToZoomEnabled(true);
+        expenseBarChart.setPinchZoom(true);
+        barDataSet.setColor(Color.parseColor("#B71C1C"));
+        expenseBarChart.invalidate(); // refresh
+    }
+
+    private void revenueCharts() {
+        /**
+         * Pie chart
+         */
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(18.5f, "Clothing"));
+        entries.add(new PieEntry(26.7f, "Gas"));
+        entries.add(new PieEntry(24.0f, "Sports"));
+        entries.add(new PieEntry(30.8f, "Food"));
+
+        PieDataSet set = new PieDataSet(entries, "");
+
+        set.setColors(new int[]{R.color.green, R.color.yellow, R.color.red, R.color.blue}, getContext());
+        set.setValueTextSize(0.1f);
+
+        PieData data = new PieData(set);
+
+        Description description = new Description();
+        description.setEnabled(false);
+
+        CharSequence title = "EXPENSES";
+
+        revenuePieChart.setData(data);
+        revenuePieChart.setDescription(description);
+        revenuePieChart.setCenterText(title);
+        revenuePieChart.notifyDataSetChanged();
+        revenuePieChart.invalidate(); // refresh
+
+        /**
+         * Bar Chart
+         */
+        List<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0f, 30f));
+        barEntries.add(new BarEntry(1f, 80f));
+        barEntries.add(new BarEntry(2f, 60f));
+        barEntries.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        barEntries.add(new BarEntry(5f, 70f));
+        barEntries.add(new BarEntry(6f, 60f));
+        BarDataSet barDataSet = new BarDataSet(barEntries, "BarDataSet");
+
+        XAxis xAxis = revenueBarChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setTextSize(8f);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelRotationAngle(-45);
+
+        YAxis right = revenueBarChart.getAxisRight();
+        right.setDrawLabels(false); // no axis labels
+        right.setDrawAxisLine(false); // no axis line
+        right.setDrawGridLines(false); // no grid lines
+        right.setDrawZeroLine(true);
+
+        YAxis left = revenueBarChart.getAxisLeft();
+        left.setDrawLabels(false); // no axis labels
+        left.setDrawAxisLine(false); // no axis line
+        left.setDrawGridLines(false); // no grid lines
+        left.setDrawZeroLine(true);
+
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.9f); // set custom bar width
+        revenueBarChart.setData(barData);
+        revenueBarChart.setFitBars(true); // make the x-axis fit exactly all bars
+        revenueBarChart.setDrawGridBackground(false);
+        revenueBarChart.setDescription(description);
+        revenueBarChart.animateXY(2000, 2000);
+        revenueBarChart.setScaleEnabled(true);
+        revenueBarChart.setDrawValueAboveBar(true);
+        revenueBarChart.setDrawGridBackground(false);
+        revenueBarChart.setDoubleTapToZoomEnabled(true);
+        revenueBarChart.setPinchZoom(true);
+        barDataSet.setColor(Color.parseColor("#606d5b"));
+        revenueBarChart.invalidate(); // refresh
     }
 
     @Override
