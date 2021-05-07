4a0cd51ccc4b6e845009c72bd14f63555ce325a7
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-05-07 16:41:00 -0400

    Added two separate views to my branch. Now, I need to do the coding for making one visible and the other one invisible.

--- app/src/main/java/home/project/group/financetracker/ui/home/HomeFragment.java
+++ app/src/main/java/home/project/group/financetracker/ui/home/HomeFragment.java
@@ -1,22 +1,42 @@
 package home.project.group.financetracker.ui.home;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.TrafficStats;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import home.project.group.financetracker.EntityClass.CategoriesModel;
 import home.project.group.financetracker.R;
 import home.project.group.financetracker.Utility.Theme;
+import home.project.group.financetracker.ui.transaction.DatabaseClass;
+import home.project.group.financetracker.ui.transaction.TransactionFragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+
+    Button addIcon;
     private TextView dateTimeDisplay;
     private Calendar calendar;
     private SimpleDateFormat dateFormat;
@@ -24,13 +44,14 @@ public class HomeFragment extends Fragment {
     private String today;
     private int dd, mm, yy;
 
-    @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         Fragment fragment = this;
         View root = Theme.themeDecider(inflater, fragment).inflate(R.layout.fragment_home, container, false);
 
+        addIcon = (Button) root.findViewById(R.id.btn_add_icon);
         dateTimeDisplay = root.findViewById(R.id.txt_Today_Date);
         final Calendar cal = Calendar.getInstance();
         dd = cal.get(Calendar.DAY_OF_MONTH);
@@ -49,8 +70,42 @@ public class HomeFragment extends Fragment {
         /* I can't add with a name until we complete the registration UI and development. For now, I just have a message using the design code. - Nick */
 
         /*------------------------------*/
+
+
+        addIcon.setOnClickListener(this);
+
+
         return root;
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_add_icon:
+
+
+                super.onResume();
+
+                 break;
+
+             default:
+                break;
+        }
+    }
 }
+
+
+
+  /*
+
+          View myView = view.findViewById(R.id.navigation_transaction);
+                myView.performClick();
+
+   /*   MenuItem menuItem = (MenuItem) view.findViewById(R.id.navigation_transaction);
+                menuItem.setActionView(R.id.navigation_transaction);
+
+    MenuItem actionRestart = (MenuItem) view.findViewById(R.id.navigation_transaction);
+                actionRestart.getActionView();*/
+
+
